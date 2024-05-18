55f57c692068bab823ed7ad519490e3d755f53a4
Rezoan Shakil Prince <prince.cuet.77@gmail.com>
2024-05-18 22:36:37 +0600

    Working

---
+++ Coding-Addict/Nuggests/1.map.js
@@ -0,0 +1,21 @@
+// Returns a new array
+// Does not change the size of original array (unlike filter)
+// Uses values from original array when making new one
+
+const people = [
+  {
+    name: 'bob',
+    age: 20,
+    position: 'developer',
+  },
+  {
+    name: 'anna',
+    age: 25,
+    position: 'designer',
+  },
+  {
+    name: 'susy',
+    age: 30,
+    position: 'the boss',
+  },
+]
