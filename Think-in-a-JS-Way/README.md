6c4209e2cc1200c3482927e6e8b262b772908df4
Rezoan Shakil Prince <prince.cuet.77@gmail.com>
2024-09-03 22:08:55 +0600

    working on js prototype

---
+++ Think-in-a-JS-Way/README.md
@@ -0,0 +1,8 @@
+- [Think in a JS Way](#think-in-a-js-way)
+  - [Prototypes](#prototypes)
+
+# Think in a JS Way
+
+## Prototypes
+
+- `prototype` is a property of an object that points to an object
---
+++ Think-in-a-JS-Way/prototypes.js
@@ -0,0 +1,26 @@
+const personMethods = {
+  eat() {
+    console.log('Person is eating.');
+  },
+
+  sleep() {
+    console.log('Person is sleeping.');
+  },
+};
+
+// constructor function
+// all the functions by default constructor functions
+// convention: write capital letter
+function Person(name, age) {
+  let person = {};
+
+  person.name = name;
+  person.age = age;
+  person.eat = personMethods.eat; // Object reference
+  person.sleep = personMethods.sleep; // Object reference
+
+  return person;
+}
+
+const sakib = Person('sakib', 32);
+const tamim = Person('tamim', 22);
