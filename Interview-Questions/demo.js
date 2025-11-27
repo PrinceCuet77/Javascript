11edfec00298a57f053c65c91a3c7f82d12eca18
Rezoan Shakil Prince <rezoan.shakil@bjitgroup.com>
2025-11-27 17:16:06 +0600

    Interview preparation day -2

--- Interview-Questions/demo.js
+++ Interview-Questions/demo.js
@@ -1,17 +1,58 @@
-class Person {
-  constructor(fname, lname, contact) {
-    this.fname = fname;
-    this.lname = lname;
-    this.contact = contact;
-  }
+const p1 = {
+  fname: 'Rezoan',
+  lname: 'Shakil',
+  age: 12,
+};
 
-  getName = function () {
-    console.log(this.fname + ' ' + this.lname);
-  };
+const p1Proxy = new Proxy(p1, {
+  get: function (target, property) {
+    if (property in target) {
+      // return target[property];
+      return Reflect.get(target, property);
+    }
+    return false;
+  },
+  set: function (target, property, value) {
+    if (!(property in target)) {
+      throw new Error(`Property ${property} does not exist`);
+    }
+
+    switch (property) {
+      case 'fname':
+      case 'lname':
+        if (typeof value !== 'string') {
+          throw new Error(`${property} must be a string`);
+        }
+        break;
+      case 'age':
+        if (typeof value !== 'number') {
+          throw new Error(`${property} must be a number`);
+        }
+        if (value < 0 || value > 150) {
+          throw new Error(`${property} must be between 0 and 150`);
+        }
+        break;
+    }
+    // target[property] = value;
+    Reflect.set(target, property, value);
+  },
+});
+
+console.log(p1Proxy.age); // Output: 12
+
+p1Proxy.fname = 'John'; // Valid
+p1Proxy.age = 30; // Valid
+
+try {
+  p1Proxy.age = -5; // Invalid
+} catch (e) {
+  console.error(e.message); // Output: age must be between 0 and 150
 }
 
-const person1 = new Person('Rezoan', 'Shakil', '9999');
-console.log(person1);
+try {
+  p1Proxy.lname = 123; // Invalid
+} catch (e) {
+  console.error(e.message); // Output: lname must be a string
+}
 
-const person2 = new Person('Shakib', 'Al-hasan', '8888');
-console.log(person2);
+console.log(p1Proxy); // Output: { fname: 'John', lname: 'Shakil', age: 30 }
